0205d4f6de7a5c268bc39c20f0a42fc9ccfdff48
George Biros <tg456894@login3.ranger.tacc.utexas.edu>
2009-02-27 17:24:01 -0600

    dummy

---
+++ examples/src/drivers/buildRgDA.C
@@ -0,0 +1,48 @@
+
+/**
+  @file buildRgDA.C
+  @author Rahul Sampath, rahul.sampath@gmail.com
+  */
+
+#include "mpi.h"
+#include "petsc.h"
+#include "sys.h"
+#include "parUtils.h"
+#include "TreeNode.h"
+#include "colors.h"
+#include "oda.h"
+#include "externVars.h"
+#include "dendro.h"
+
+int main(int argc, char ** argv ) {	
+  int size, rank;
+  bool compressLut=false;
+  unsigned int regLev = 2;
+  std::vector<ot::TreeNode> balOct;
+
+  PetscInitialize(&argc,&argv,"options",NULL);
+  ot::RegisterEvents();
+  ot::DA_Initialize(MPI_COMM_WORLD);
+
+  MPI_Comm_size(MPI_COMM_WORLD,&size);
+  MPI_Comm_rank(MPI_COMM_WORLD,&rank);
+  if(argc < 2) {
+    std::cerr << "Usage: " << argv[0] << "regLev compressLut[0] " << std::endl;
+    return -1;
+  }
+
+  regLev = atoi(argv[1]);
+  if(argc > 2) { compressLut = (bool)(atoi(argv[2]));}
+
+  ot::createRegularOctree(balOct, regLev, 3, 30, MPI_COMM_WORLD);
+
+  //ODA ...
+  MPI_Barrier(MPI_COMM_WORLD);	
+  assert(!(balOct.empty()));
+  ot::DA da(balOct, MPI_COMM_WORLD, MPI_COMM_WORLD, compressLut);
+  balOct.clear();
+
+  ot::DA_Finalize();
+  PetscFinalize();
+}//end function
+
