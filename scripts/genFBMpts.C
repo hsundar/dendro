34f4288575daf6a5dd428d18777ae7155cfa4c24
Rahul Sampath <rahul.sampath@gmail.com>
2009-03-09 18:27:03 -0400

    dummy

---
+++ scripts/genFBMpts.C
@@ -0,0 +1,52 @@
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <cassert>
+
+#define __PI__ 3.14159265
+
+void writePtsToFile( const double* pts, const unsigned int ptsLen, char* filename) {
+  FILE*  outfile = fopen(filename,"w");
+  if(ptsLen >0) {
+    unsigned int numPts = ptsLen/3;
+    fwrite(&numPts, sizeof(unsigned int),1,outfile);
+    fwrite(pts, sizeof(double),ptsLen,outfile);
+  }
+  fclose(outfile);
+}//end function
+
+int main(int argc, char **argv) {
+  if (argc < 4) {
+    std::cerr << "<exe> angInc1 angInc2 fbmR" << std::endl;
+    return -1;
+  }
+
+  double angInc1 = atof(argv[1]);
+  double angInc2 = atof(argv[2]);
+  double fbmR = atof(argv[3]);
+
+  std::vector<double> outPts;
+
+  for(double ang1 = 0; ang1 < (2*__PI__); ang1 += angInc1) {
+    for(double ang2 = 0; ang2 < __PI__; ang2 += angInc2) {
+      double x = 0.5 + fbmR*cos(ang1)*sin(ang2);
+      double y = 0.5 + fbmR*sin(ang1)*sin(ang2);
+      double z = 0.5 + fbmR*cos(ang2);
+      outPts.push_back(x);
+      outPts.push_back(y);
+      outPts.push_back(z);
+    }
+  }
+
+  std::cout<<"Writing "<<(outPts.size()/3)<<" points."<<std::endl;
+
+  writePtsToFile(&(*outPts.begin()), outPts.size(), "fbmInp.pts");
+
+  outPts.clear();
+
+  return 0;
+}
+
+
+
