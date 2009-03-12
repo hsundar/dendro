50bbc9a18d40dea739bb90a0258365a8cf1d617d
Rahul Sampath <rahul.sampath@gmail.com>
2009-03-12 12:35:33 -0400

    dummy

--- examples/src/drivers/tstDirichlet.C
+++ examples/src/drivers/tstDirichlet.C
@@ -165,15 +165,12 @@ int main(int argc, char ** argv ) {
   PetscReal maxNormErr;
   VecNorm(solTrue, NORM_INFINITY, &maxNormErr);
 
-  double l2err = ComputeFBMerror(damg[nlevels - 1], DAMGGetx(damg));
-
   VecDestroy(solTrue);
 
   if(!rank) {
     std::cout<<" Total Setup Time: "<<(setupEndTime - setupStartTime)<<std::endl;
     std::cout<<" Total Solve Time: "<<(solveEndTime - solveStartTime)<<std::endl;
     std::cout<<" maxNormErr (Pointwise): "<<maxNormErr<<std::endl;
-    std::cout<<" l2err: "<<l2err<<std::endl;
   }
 
   destroyLmatType2(LaplacianType2Stencil);
